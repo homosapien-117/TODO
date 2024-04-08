5b20108f6662ebeeab93d9f2852f1f7246a91096
AJAY KRISHNA <ajaykrishna2000117@gmail.com>
2024-04-08 10:34:15 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState("");
+  const deleteTodo = (id) => {
+    setTodos(toDos.filter((todo) => todo.id !== id));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Not OneDay, it's Day One🌝 ☕️ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(event) => setTodo(event.target.value)}
+          type="text"
+          placeholder="🖊 Add item..."
+        />
+        <i
+          className="fas fa-plus"
+          onClick={() =>{
+            setTodos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+            setTodo("");
+          }   
+          }
+        ></i>
+      </div>
+      <div className="todos">
+        {toDos.map((obj) => {
+          return (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={(event) => {
+                    console.log(event.target.checked);
+                    console.log(obj);
+                    setTodos(toDos.filter(obj2=>{
+                      if (obj2.id===obj.id) {
+                        obj2.status=event.target.checked
+                      }
+                      return obj2
+                    }))
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times" onClick={() => deleteTodo(obj.id)}></i>
+              </div>
+            </div>
+          );
+        })}
+        {
+          toDos.map((obj)=>{
+            if (obj.status) {
+              return (<h1>{obj.text}</h1>)
+            }
+            return null
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/employee.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function employee(props) {
+  return (
+    <div>
+      <h1>{props.Name} {props.Age}</h1>
+    </div>
+  )
+}
+
+export default employee
